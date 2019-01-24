5fa2f8987867758fe67689d72e19030c7a5d3ecf
TuDi <superdi.cdi@gmail.com>
2019-01-24 22:34:58 +0800

    todo

---
+++ src/todo/footer.jsx
@@ -0,0 +1,16 @@
+import '../assets/styles/footer.styl'
+
+export default {
+    data(){
+        return {
+            author: 'Tudi'
+        }
+    },
+    render(){
+        return(
+            <div id="footer">
+                <span>Writter by Tudi</span>
+            </div>
+        )
+    }
+}
